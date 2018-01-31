d55fdca0e95499cd18f65d0138ca67bcfec61f60
Dewey Dunnington <dewey@fishandwhistle.net>
2018-01-31 18:55:46 -0400

    fix detail pages

--- lims/views/detail.py
+++ lims/views/detail.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 from .. import models
 from .accounts import LimsLoginMixin
 from .list import query_string_filter, default_published_filter
+from .actions import SAMPLE_ACTIONS, LOCATION_ACTIONS
 
 
 class DetailViewWithTablesBase(generic.DetailView):
@@ -19,45 +20,51 @@ class DetailViewWithTablesBase(generic.DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        # setup the child samples list
-        samples = query_string_filter(
-            default_published_filter(self.get_sample_queryset().order_by('-modified'), self.request.user),
-            self.request.GET,
-            prefix='sample_'
-        )
-        sample_paginator = Paginator(samples, per_page=100)
-        context['sample_page_obj'] = sample_paginator.page(self.request.GET.get('sample_page', 1))
-        context['sample_page_kwarg'] = 'sample_page'
+        sample_queryset = self.get_sample_queryset()
 
-        # add some actions for the child samples
-        context['sample_actions'] = [
-            {'value': 'delete-samples', 'label': 'Delete samples'},
-            {'value': 'print-barcodes', 'label': 'Print barcodes'},
-            {'value': 'export-samples', 'label': 'Export selected samples'}
-        ]
+        if sample_queryset is not None:
+            # setup the child samples list
+            samples = query_string_filter(
+                default_published_filter(self.get_sample_queryset().order_by('-modified'), self.request.user),
+                self.request.GET,
+                prefix='sample_'
+            )
+            sample_paginator = Paginator(samples, per_page=100)
+            context['sample_page_obj'] = sample_paginator.page(self.request.GET.get('sample_page', 1))
+            context['sample_page_kwarg'] = 'sample_page'
 
-        # setup the child locations list
-        locations = query_string_filter(
-            self.get_location_queryset().order_by('-modified'),
-            self.request.GET,
-            prefix='location_'
-        )
-        location_paginator = Paginator(locations, per_page=100)
-        context['location_page_obj'] = location_paginator.page(self.request.GET.get('location_page', 1))
-        context['location_page_kwarg'] = 'location_page'
+            # add some actions for the child samples
+            context['sample_actions'] = SAMPLE_ACTIONS
 
-        # add some actions for the child locations
-        context['location_actions'] = [
-            {'value': 'delete-locations', 'label': 'Delete selected locations'},
-            {'value': 'export-locations', 'label': 'Export selected locations'}
-        ]
+        location_queryset = self.get_location_queryset()
+
+        if location_queryset is not None:
+            # setup the child locations list
+            locations = query_string_filter(
+                self.get_location_queryset().order_by('-modified'),
+                self.request.GET,
+                prefix='location_'
+            )
+            location_paginator = Paginator(locations, per_page=100)
+            context['location_page_obj'] = location_paginator.page(self.request.GET.get('location_page', 1))
+            context['location_page_kwarg'] = 'location_page'
+
+            # add some actions for the child locations
+            context['location_actions'] = LOCATION_ACTIONS
 
         return context
 
 
-class SampleDetailView(LimsLoginMixin, generic.DetailView):
+class SampleDetailView(LimsLoginMixin, DetailViewWithTablesBase):
     template_name = 'lims/sample_detail.html'
     model = models.Sample
+
+    def get_sample_queryset(self):
+        return self.object.children.all()
+
+    def get_location_queryset(self):
+        return None
+
 
 
 class LocationDetailView(LimsLoginMixin, DetailViewWithTablesBase):
