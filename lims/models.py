632e4defc1b0129f2014d02b0017a7aefa6b0658
Dewey Dunnington <dewey@fishandwhistle.net>
2018-09-30 14:51:40 -0300

    fix merge breaks

--- lims/models.py
+++ lims/models.py
@@ -56,10 +56,10 @@ class BaseObjectModel(models.Model):
     recursive_depth = models.IntegerField(default=0, editable=False)
 
     geometry = models.TextField(blank=True, validators=[validate_wkt, ])
-    xmin = models.FloatField(editable=False, blank=True, null=True, default=None)
-    xmax = models.FloatField(editable=False, blank=True, null=True, default=None)
-    ymin = models.FloatField(editable=False, blank=True, null=True, default=None)
-    ymax = models.FloatField(editable=False, blank=True, null=True, default=None)
+    geo_xmin = models.FloatField(editable=False, blank=True, null=True, default=None)
+    geo_xmax = models.FloatField(editable=False, blank=True, null=True, default=None)
+    geo_ymin = models.FloatField(editable=False, blank=True, null=True, default=None)
+    geo_ymax = models.FloatField(editable=False, blank=True, null=True, default=None)
 
     def _should_update_slug(self):
         return not self.pk and not self.slug
@@ -94,10 +94,10 @@ class BaseObjectModel(models.Model):
 
         # cache location info
         bounds = wkt_bounds(self.geometry)
-        self.xmin = bounds['xmin']
-        self.xmax = bounds['xmax']
-        self.ymin = bounds['ymin']
-        self.ymax = bounds['ymax']
+        self.geo_xmin = bounds['xmin']
+        self.geo_xmax = bounds['xmax']
+        self.geo_ymin = bounds['ymin']
+        self.geo_ymax = bounds['ymax']
         super().save(*args, **kwargs)
 
     def auto_slug_use(self):
